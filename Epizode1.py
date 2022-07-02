fa430a1988e2ed15a92c440b6b22e076542f8319
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2022-07-02 17:13:56 +0300

    fix: Puzzle 1 - counted was giving wrong data because of sqlalchemy

--- Epizode1.py
+++ Epizode1.py
@@ -34,9 +34,12 @@
 #  Identify all blood samples (in the population register) that contain one or more pico-bots of generation 1.
 # Solution code: the sum of the IDs of all people in question.
 import collections
+from statistics import mean
+import math
+import matplotlib.pyplot as plt
 import pandas as pd
-from sqlalchemy import false, true
-import re
+import numpy as np
+
 ## Open file
 population = open('population.txt').read()
 populationSplit = population.split("\n\n")
@@ -67,7 +70,7 @@ print(populationDf['BloodSample'][12731])
 
 ## Find pico in Blood Sample
 sampleColumns = [str]
-IDList = []
+idList = []
 for i in range(len(populationDf)):
     sampleRows = str(populationDf['BloodSample'][i]).replace('  +--------+  |','').replace('|  +--------+','').split('|  |')
     # if i == 0: print(populationDf['BloodSample'][i], sampleRows)
@@ -78,28 +81,95 @@ for i in range(len(populationDf)):
             sample = sample + sampleRows[k][j]
         sampleColumns.append(sample)
         # if j == 0: print(sampleColumns)
-    counted = false
+    counted = False
     for l in sampleRows:
         if 'pico' in l:
-            IDList.append(int(populationDf['ID'][i]))
-            counted = true
+            idList.append(int(populationDf['ID'][i]))
+            counted = True
             break
         elif 'ocip' in l:
-            IDList.append(int(populationDf['ID'][i]))
-            counted = true
+            idList.append(int(populationDf['ID'][i]))
+            counted = True
             break
     if(not counted):
         for m in sampleColumns:
             if 'pico' in m:
-                IDList.append(int(populationDf['ID'][i]))
+                idList.append(int(populationDf['ID'][i]))
                 break
             elif 'ocip' in m:
-                IDList.append(int(populationDf['ID'][i]))
+                idList.append(int(populationDf['ID'][i]))
                 break
 sum = 0
-for i in range(len(IDList)):
-    sum = sum + IDList[i]
+for i in range(len(idList)):
+    sum = sum + idList[i]
 print('The sum of the IDs of all people with pico: ',sum) 
 
 ## find duplicates
-print([item for item, count in collections.Counter(IDList).items() if count > 1])
+print([item for item, count in collections.Counter(idList).items() if count > 1])
+
+# 1.2 Two-Dimensional Galaxy
+# In our (fictitous) universe, a galaxy is an almost two-dimensional object. 
+# While, in two of its dimensions, planets are spread out very far, in its third dimension, 
+# a galaxy is very flat. If we place a two-dimensional plane into the galaxy, then the distance 
+# of each planet from this plane is smaller than 2 galactic units (galactic units are length units in our fictitous universe).
+# However, not all planets follow this rule. There is a special type of planet, called outlier planet. 
+# These planets have a significantly larger distance from the galaxy's plane: they are at least 10 galactic units away from the plane.
+# You are given the coordinates of all planets in the galaxy (given as (x,y,z) in galactic units) in
+#  file galaxy_map.txt. Unfortunately, due to historic reasons, the universes coordinate system is not aligned with the galaxy's plane.
+# Jelly Jones' shoulder tattoo says "We are Outliers! We don't fit!". The space pirate must be living on one of the outlier planets. 
+# First, identify all outlier planets (in galaxy_map.txt) and then find all inhabitants that have an outlier planet as their home planet (in population.txt).
+# Solution code: the sum of the IDs of all people in question.
+def CalculateDistance(point, planePoint, normalVector = np.array([1,1,1])):
+    # a plane is a*x+b*y+c*z+d=0
+    # [a,b,c] is the normal. Thus, we have to calculate
+    # d and we're set
+    d = -np.dot(planePoint,normalVector) #
+    distance = abs(np.dot(point, normalVector) + d)/math.sqrt(normalVector[0]**2 + normalVector[1]**2 + normalVector[2]**2)
+    return distance
+
+galaxiMap = open('galaxy_map.txt')
+
+listPlanetCoord = []
+for line in galaxiMap:
+    listPlanetCoord.append(line.rstrip().replace(' ','').split(':'))
+print(listPlanetCoord[0])
+listPlanetNames = []
+listXCoord = []
+listYCoord = []
+listZCoord = []
+for coord in listPlanetCoord:
+    x,y,z = (str(coord[1]).replace('(','').replace(')','').split(','))
+    listPlanetNames.append(coord[0])
+    listXCoord.append(float(x))
+    listYCoord.append(float(y))
+    listZCoord.append(float(z))
+print(listXCoord[0], listYCoord[0], listZCoord[0])
+
+## make galaxy dataframe 
+galaxyDf = pd.DataFrame({'PlanetName': listPlanetNames, 
+    'x': listXCoord, 
+    'y': listYCoord, 
+    'z': listZCoord})
+print(galaxyDf)
+
+# define the plane with point and normal vector
+pointPlane  = np.array([20, 40, 70])
+normal = np.array([1, 1, 1])
+print(pointPlane)
+
+## calculate distance from plane
+outlierPlanetList = []
+for i in range(len(galaxyDf)):
+    point = (float(galaxyDf['x'][i]),float(galaxyDf['y'][i]),float(galaxyDf['z'][i]))
+    distance = CalculateDistance(point,pointPlane, normal) 
+    if distance >= 10:
+        # print('calculated distance : ',distance)
+        outlierPlanetList.append(galaxyDf['PlanetName'][i])
+# print(outlierPlanetList)
+sum = 0
+for i in range(len(populationDf)):
+    for j in range(len(outlierPlanetList)):
+        if populationDf['HomePlanet'][i].replace(' ','') == outlierPlanetList[j]:
+            sum = sum + int(populationDf['ID'][i])
+            break
+print('The sum of the IDs of all people with outlier planet: ',sum) 
